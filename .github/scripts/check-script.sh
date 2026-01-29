420bc02ef7006633356d7b65e2f38d19e89f4c63
Mohammed Nizamuddin <Mohammed.Nizamuddin+CPSI@trubridge.com>
2026-01-29 12:48:55 -0500

    check base and head

--- .github/scripts/check-script.sh
+++ .github/scripts/check-script.sh
@@ -5,8 +5,8 @@ git fetch origin main:main
 
 CHANGED=false
 # Get list of changed YAML files in the specific path
-# CHANGED_FILES=$(git diff --name-only "${{ github.base_ref }}".."${{ github.head_ref }}" -- 'ansible-aad/*.yaml')
-CHANGED_FILES=$(git diff --name-only main..cop -- 'ansible-aad/*.yaml')
+CHANGED_FILES=$(git diff --name-only "${{ github.base_ref }}".."${{ github.head_ref }}" -- 'ansible-aad/*.yaml')
+# CHANGED_FILES=$(git diff --name-only main..cop -- 'ansible-aad/*.yaml')
 
 for FILE in $CHANGED_FILES; do
   # git show "${{ github.base_ref }}:$FILE" > base.yaml 2>/dev/null || echo "{}" > base.yaml
